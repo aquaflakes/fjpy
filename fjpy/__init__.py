9607ec1cc3a1f970101b0ae3031d4b2db970cc51
zhu <aquaflakes@gmail.com>
2018-07-02 17:12:53 +0100

    update

--- fjpy/__init__.py
+++ fjpy/__init__.py
@@ -1,10 +1,10 @@
-from __future__ import absolute_import
+# from __future__ import absolute_import
 import pandas as pd
 import numpy as np
 
 
 
-# from . import tester
+# from . import tester 
 
 # Also importable from root
 from .tester import printme
