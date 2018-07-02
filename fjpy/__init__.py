608b22a23c88417cc17a6cf78fc9112715fdd62f
zhu <aquaflakes@gmail.com>
2018-07-02 17:20:06 +0100

    update

--- fjpy/__init__.py
+++ fjpy/__init__.py
@@ -1,15 +1,13 @@
 # from __future__ import absolute_import
-import pandas as pd
-import numpy as np
 
+# import pandas as pd
+# import numpy as np
 
-
-# from . import tester 
+# from . import tester
 
 # Also importable from root
 from .tester import printme
 
 
 __all__ = [s for s in dir() if not s.startswith("_")]
-print(__all__)
 __version__ = '2.2.0'
