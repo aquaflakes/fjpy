5604e8e32ec166e6cdb75a85ae2af03b434b6610
zhu <aquaflakes@gmail.com>
2018-07-02 13:33:06 +0100

    v1

--- setup.py
+++ setup.py
@@ -15,21 +15,10 @@ setup(
     version='0.1',
     # The license can be anything you like
     license='MIT',
-    description='An example of a python package from pre-existing code',
+    description='some utilities',
     # We will also need a readme eventually (there will be a warning)
     long_description=open('README.md').read(),
     # scripts=['']
 )
 
 
-# from setuptools import setup
-# from setuptools import find_packages
-# setup(
-#      name='fjpy',    # This is the name of your PyPI-package.
-#      version='1.0',                          # Update the version number for new releases
-#      description='',
-#      packages=['fjpy'],#find_packages(),
-#      url='https://github.com/aquaflakes/fjpy',
-#      install_requires=['pipimport','numpy','ggplot','pandas','SciPy'],
-#      scripts=['']                  # The name of your scipt, and also the command you'll be using for calling it
-# )
