8ed81d117eae2a9733d9931deccaba6291f2ef60
zhu <aquaflakes@gmail.com>
2018-07-02 13:26:32 +0100

    v1

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@
 # )
 
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
@@ -19,7 +20,7 @@ setup(
     url='https://github.com/jladan/package_demo',
     author='Fangjie Zhu',
     # Needed to actually package something
-    packages=['fjpy'],
+    packages=find_packages(),#['fjpy'],
     # Needed for dependencies
     install_requires=['pipimport','numpy','ggplot','pandas','SciPy'],
     # *strongly* suggested for sharing
@@ -28,5 +29,5 @@ setup(
     license='MIT',
     description='An example of a python package from pre-existing code',
     # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
