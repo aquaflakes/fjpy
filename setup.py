0d19c758e031c52800cb0d9ab3a34e136fe93c20
zhu <aquaflakes@gmail.com>
2018-07-02 13:29:27 +0100

    v1

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python
-# from setuptools import setup
-# from setuptools import find_packages
-# setup(
-#      name='fjpy',    # This is the name of your PyPI-package.
-#      version='1.0',                          # Update the version number for new releases
-#      description='',
-#      packages=['fjpy'],#find_packages(),
-#      url='https://github.com/aquaflakes/fjpy',
-#      install_requires=['pipimport','numpy','ggplot','pandas','SciPy'],
-#      scripts=['']                  # The name of your scipt, and also the command you'll be using for calling it
-# )
-
 from setuptools import setup
 from setuptools import find_packages
 
@@ -32,3 +20,16 @@ setup(
     long_description=open('README.md').read(),
     scripts=['']
 )
+
+
+# from setuptools import setup
+# from setuptools import find_packages
+# setup(
+#      name='fjpy',    # This is the name of your PyPI-package.
+#      version='1.0',                          # Update the version number for new releases
+#      description='',
+#      packages=['fjpy'],#find_packages(),
+#      url='https://github.com/aquaflakes/fjpy',
+#      install_requires=['pipimport','numpy','ggplot','pandas','SciPy'],
+#      scripts=['']                  # The name of your scipt, and also the command you'll be using for calling it
+# )
