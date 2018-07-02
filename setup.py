942cc85dd7f4a4d8c1f23336d191d97bd3a38152
zhu <aquaflakes@gmail.com>
2018-07-02 13:22:08 +0100

    v1

--- setup.py
+++ setup.py
@@ -5,8 +5,28 @@ setup(
      name='fjpy',    # This is the name of your PyPI-package.
      version='1.0',                          # Update the version number for new releases
      description='',
-     packages=find_packages(),
+     packages=['fjpy'],#find_packages(),
      url='https://github.com/aquaflakes/fjpy',
      install_requires=['pipimport','numpy','ggplot','pandas','SciPy'],
      scripts=['']                  # The name of your scipt, and also the command you'll be using for calling it
 )
+
+# from setuptools import setup
+#
+# setup(
+#     # Needed to silence warnings (and to be a worthwhile package)
+#     name='Measurements',
+#     # url='https://github.com/jladan/package_demo',
+#     author='Fangjie Zhu',
+#     # Needed to actually package something
+#     packages=['fjpy'],
+#     # Needed for dependencies
+#     install_requires=['numpy'],
+#     # *strongly* suggested for sharing
+#     version='0.1',
+#     # The license can be anything you like
+#     license='MIT',
+#     description='An example of a python package from pre-existing code',
+#     # We will also need a readme eventually (there will be a warning)
+#     # long_description=open('README.txt').read(),
+# )
